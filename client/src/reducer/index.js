10c51316a4da67deff7015790ea6874d67ce877e
Mariana Salez <mariana.i.salez@gmail.com>
2022-09-12 23:21:56 -0300

    Primera aprox a react y creacion del nav

---
+++ client/src/reducer/index.js
@@ -0,0 +1,25 @@
+
+// Importa las actions types que necesites acá:
+//import {} from "../actions";
+
+
+const initialState = {
+Loading: true,
+  Recipes: [],
+  Diets: [],
+};
+
+const rootReducer = (state = initialState, action) => {
+  /*switch(action.type) {
+      // Acá va tu código:
+      case :
+        return {}
+      
+      default:
+        return state
+
+     
+  };*/
+};
+
+export default rootReducer;
